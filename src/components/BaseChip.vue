1aefff0f27934c41478618f6837215ff52baafe8
Eterion <Eterion@users.noreply.github.com>
2022-12-26 21:08:33 +0100

    Chip component

---
+++ src/components/BaseChip.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core';
+
+const props = defineProps<{
+  /**
+   * Controls active state.
+   */
+  active?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:active', active?: boolean): void;
+}>();
+
+const active = useVModel(props, 'active', emit);
+</script>
+
+<template>
+  <button type="button" @click="active = !active">
+    <slot />
+    ({{ active ? 'true' : 'false' }})
+  </button>
+</template>
