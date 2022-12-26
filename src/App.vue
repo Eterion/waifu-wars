ca99f368dbe7a5c89845034c10af00b1e37f84f2
Eterion <Eterion@users.noreply.github.com>
2022-12-26 20:55:17 +0100

    App first steps

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import SearchBox from './components/SearchBox.vue';
+
+const searchQuery = ref('');
+</script>
+
 <template>
-  <div />
+  <div>
+    <h1>Waifu Wars</h1>
+    <p>
+      We all have our favorite waifus. And of course we can scientifically prove
+      it by comparing them to inferior trash.
+    </p>
+    <SearchBox v-model="searchQuery" />
+    <div>Search powered by Anilist public api.</div>
+  </div>
 </template>
