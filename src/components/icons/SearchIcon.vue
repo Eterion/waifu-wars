e17d70a74edf33b86761e11bb20f51780f83cf31
Eterion <Eterion@users.noreply.github.com>
2022-12-26 20:54:42 +0100

    Search icon

---
+++ src/components/icons/SearchIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { iconProps, useIcon } from '@/composables/useIcon';
+
+const props = defineProps(iconProps);
+const { height, width } = useIcon(props);
+</script>
+
+<template>
+  <svg
+    :height="height"
+    :width="width"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    stroke="currentColor"
+    viewBox="0 0 24 24">
+    <circle cx="11" cy="11" r="8" />
+    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+  </svg>
+</template>
