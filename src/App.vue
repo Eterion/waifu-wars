37f290ab9ff5d4ad0233ae51a61fb36b8948d04d
Eterion <Eterion@users.noreply.github.com>
2022-12-26 21:01:35 +0100

    Add display toggle

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import GridIcon from './components/icons/GridIcon.vue';
+import ListIcon from './components/icons/ListIcon.vue';
 import SearchBox from './components/SearchBox.vue';
 
 const searchQuery = ref('');
+const isGrid = ref(false);
 </script>
 
 <template>
   <div>
-    <h1>Waifu Wars</h1>
-    <p>
-      We all have our favorite waifus. And of course we can scientifically prove
-      it by comparing them to inferior trash.
-    </p>
-    <SearchBox v-model="searchQuery" />
-    <div>Search powered by Anilist public api.</div>
+    <div>
+      <h1>Waifu Wars</h1>
+      <p>
+        We all have our favorite waifus. And of course we can scientifically
+        prove it by comparing them to inferior trash.
+      </p>
+    </div>
+    <div>
+      <SearchBox v-model="searchQuery" />
+      <div>Search powered by Anilist public api.</div>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <button type="button" @click="isGrid = !isGrid">
+            <ListIcon v-if="isGrid" />
+            <GridIcon v-else />
+          </button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
