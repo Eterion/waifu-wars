252746f4563558f36d9469c416465527944e3e75
Eterion <Eterion@users.noreply.github.com>
2022-12-26 20:58:26 +0100

    List icon

---
+++ src/components/icons/ListIcon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { iconProps, useIcon } from '@/composables/useIcon';
+
+const props = defineProps(iconProps);
+const { height, width } = useIcon(props);
+</script>
+
+<template>
+  <svg
+    :height="height"
+    :width="width"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    stroke="currentColor"
+    viewBox="0 0 24 24">
+    <line x1="8" y1="6" x2="21" y2="6" />
+    <line x1="8" y1="12" x2="21" y2="12" />
+    <line x1="8" y1="18" x2="21" y2="18" />
+    <line x1="3" y1="6" x2="3.01" y2="6" />
+    <line x1="3" y1="12" x2="3.01" y2="12" />
+    <line x1="3" y1="18" x2="3.01" y2="18" />
+  </svg>
+</template>
