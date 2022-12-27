2c65bc1a987d6a4e3ec626db527c3c8465d63428
Eterion <Eterion@users.noreply.github.com>
2022-12-27 11:23:50 +0100

    Moon icon

---
+++ src/components/icons/MoonIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { iconProps, useIcon } from '@/composables/useIcon';
+
+const props = defineProps(iconProps);
+const { height, width } = useIcon(props);
+</script>
+
+<template>
+  <svg
+    :height="height"
+    :width="width"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    stroke="currentColor"
+    viewBox="0 0 24 24">
+    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+  </svg>
+</template>
