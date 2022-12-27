f8e46e8679441e6761d60521b176cc133f9aac3a
Eterion <Eterion@users.noreply.github.com>
2022-12-27 11:13:25 +0100

    X icon

---
+++ src/components/icons/XIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { iconProps, useIcon } from '@/composables/useIcon';
+
+const props = defineProps(iconProps);
+const { height, width } = useIcon(props);
+</script>
+
+<template>
+  <svg
+    :height="height"
+    :width="width"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    stroke="currentColor"
+    viewBox="0 0 24 24">
+    <line x1="18" y1="6" x2="6" y2="18" />
+    <line x1="6" y1="6" x2="18" y2="18" />
+  </svg>
+</template>
