eed1d59548529ad3d801595d244c79fe92ef1dd6
Eterion <Eterion@users.noreply.github.com>
2022-12-27 11:29:19 +0100

    Sidebar and main

--- src/components/TheApp.vue
+++ src/components/TheApp.vue
@@ -4,11 +4,14 @@ import type { Character } from '@/types/Character';
 import { computed, reactive, ref } from 'vue';
 import BaseCharacter from './BaseCharacter.vue';
 import BaseChip from './BaseChip.vue';
+import GithubIcon from './icons/GithubIcon.vue';
 import GridIcon from './icons/GridIcon.vue';
 import ListIcon from './icons/ListIcon.vue';
+import MoonIcon from './icons/MoonIcon.vue';
+import ShareIcon from './icons/ShareIcon.vue';
 import SearchBox from './SearchBox.vue';
 
-const searchQuery = ref('');
+const searchQuery = ref<string>();
 const isGrid = ref(false);
 const isFilter = reactive({
   waifus: true,
@@ -59,43 +62,80 @@ const filteredCharacters = computed(() => {
 </script>
 
 <template>
-  <div>
-    <div>
-      <h1>Waifu Wars</h1>
-      <p>
-        We all have our favorite waifus. And of course we can scientifically
-        prove it by comparing them to inferior trash.
-      </p>
-    </div>
-    <div>
-      <SearchBox v-model="searchQuery" />
-      <div>Search powered by Anilist public api.</div>
-    </div>
-    <div>
-      <ul>
-        <li>
-          <button type="button" @click="isGrid = !isGrid">
-            <ListIcon v-if="isGrid" />
-            <GridIcon v-else />
-          </button>
-        </li>
-        <li>
-          <BaseChip v-model:active="isFilter.waifus">Waifus</BaseChip>
-        </li>
-        <li>
-          <BaseChip v-model:active="isFilter.husbandos">Husbandos</BaseChip>
-        </li>
-        <li>
-          <BaseChip v-model:active="isFilter.other">Other</BaseChip>
-        </li>
-      </ul>
-    </div>
-    <div>
-      <ul>
-        <li v-for="characterInfo in filteredCharacters" :key="characterInfo.id">
-          <BaseCharacter :card="isGrid" :info="characterInfo" />
-        </li>
-      </ul>
-    </div>
+  <div :class="$style.el">
+    <aside :class="$style.sidebar">
+      <div>
+        <h1>Waifu Wars</h1>
+        <p>
+          We all have our favorite waifus. And of course we can scientifically
+          prove it by comparing them to inferior trash.
+        </p>
+      </div>
+      <div>
+        <SearchBox v-model="searchQuery" />
+        <div>Search powered by Anilist public api.</div>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <button type="button" @click="isGrid = !isGrid">
+              <ListIcon v-if="isGrid" />
+              <GridIcon v-else />
+            </button>
+          </li>
+          <li>
+            <BaseChip v-model:active="isFilter.waifus">Waifus</BaseChip>
+          </li>
+          <li>
+            <BaseChip v-model:active="isFilter.husbandos">Husbandos</BaseChip>
+          </li>
+          <li>
+            <BaseChip v-model:active="isFilter.other">Other</BaseChip>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li
+            v-for="characterInfo in filteredCharacters"
+            :key="characterInfo.id">
+            <BaseCharacter :card="isGrid" :info="characterInfo" />
+          </li>
+        </ul>
+      </div>
+    </aside>
+    <main :class="$style.main">
+      <div>
+        <ul>
+          <li>
+            <button type="button">
+              <ShareIcon />
+            </button>
+          </li>
+          <li>
+            <a href="https://github.com/Eterion/waifu-wars" target="_blank">
+              <GithubIcon />
+            </a>
+          </li>
+          <li>
+            <button type="button">
+              <MoonIcon />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
+
+<style module lang="scss">
+.el {
+  display: grid;
+  grid-template-columns: 480px 1fr;
+}
+
+.sidebar,
+.main {
+  padding: 64px;
+}
+</style>
