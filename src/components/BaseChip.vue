4411197072f0aa6f6baf85051d9303e58815d643
Eterion <Eterion@users.noreply.github.com>
2022-12-28 13:35:42 +0100

    Chip color and style

--- src/components/BaseChip.vue
+++ src/components/BaseChip.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { useVModel } from '@vueuse/core';
+import type OpenColor from 'open-color';
 
 const props = defineProps<{
   /**
    * Controls active state.
    */
   active?: boolean;
+  /**
+   * Color.
+   */
+  color: Exclude<keyof OpenColor, 'black' | 'white'>;
 }>();
 
 const emit = defineEmits<{
@@ -16,8 +21,61 @@ const active = useVModel(props, 'active', emit);
 </script>
 
 <template>
-  <button type="button" @click="active = !active">
+  <button
+    :class="[$style.el, $style[color], { [$style.active]: active }]"
+    type="button"
+    @click="active = !active">
     <slot />
-    ({{ active ? 'true' : 'false' }})
   </button>
 </template>
+
+<style module lang="scss">
+@use 'open-color/open-color' as *;
+@use 'sass:math';
+@use 'sass:map';
+
+$-height: 30px;
+
+.el {
+  align-items: center;
+  background-color: var(--gray);
+  border: none;
+  border-radius: math.div($-height, 2);
+  cursor: pointer;
+  display: flex;
+  font-size: 0.8125rem;
+  height: $-height;
+  justify-content: center;
+  opacity: 0.65;
+  padding: 0 10px;
+  transition-duration: 150ms;
+  transition-property: background-color, color, opacity;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &.active {
+    background-color: var(--chip-bg);
+    color: var(--chip-text);
+    opacity: 1;
+  }
+}
+
+@each $-colors, $-key in $oc-color-list {
+  .#{$-key} {
+    &.active {
+      --chip-bg: #{map.get($-colors, '7')};
+      --chip-text: #{$oc-white};
+      :global(.dark) & {
+        @if ($-key == 'gray') {
+          --chip-bg: #{$oc-white};
+          --chip-text: #{$oc-black};
+        } @else {
+          --chip-bg: #{map.get($-colors, '5')};
+        }
+      }
+    }
+  }
+}
+</style>
