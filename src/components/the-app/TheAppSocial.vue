5061fb28d674f2142cf6aaaad4514ece28d3849e
Eterion <Eterion@users.noreply.github.com>
2022-12-28 14:18:41 +0100

    Style social links

--- src/components/the-app/TheAppSocial.vue
+++ src/components/the-app/TheAppSocial.vue
@@ -7,27 +7,58 @@ import ShareIcon from '../icons/ShareIcon.vue';
 import SunIcon from '../icons/SunIcon.vue';
 
 const colorSchemeStore = useColorSchemeStore();
-const { toggle } = colorSchemeStore;
+const { toggle: toggleColorScheme } = colorSchemeStore;
 const { isDark } = storeToRefs(colorSchemeStore);
+
+function share() {
+  alert('Not yet implemented');
+}
 </script>
 
 <template>
-  <ul>
+  <ul :class="$style.el">
     <li>
-      <button type="button">
-        <ShareIcon />
+      <button :class="$style.item" type="button" @click="share">
+        <ShareIcon :size="18" />
       </button>
     </li>
     <li>
-      <a href="https://github.com/Eterion/waifu-wars" target="_blank">
-        <GithubIcon />
+      <a
+        :class="$style.item"
+        href="https://github.com/Eterion/waifu-wars"
+        target="_blank">
+        <GithubIcon :size="18" />
       </a>
     </li>
     <li>
-      <button type="button" @click="toggle()">
-        <SunIcon v-if="isDark" />
-        <MoonIcon v-else />
+      <button :class="$style.item" type="button" @click="toggleColorScheme()">
+        <SunIcon v-if="isDark" :size="18" />
+        <MoonIcon v-else :size="18" />
       </button>
     </li>
   </ul>
 </template>
+
+<style module lang="scss">
+.el {
+  align-items: center;
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+
+.item {
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  color: var(--text-light);
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+  size: 46px;
+  &:hover {
+    color: var(--text);
+  }
+}
+</style>
