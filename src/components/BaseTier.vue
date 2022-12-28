692a074f4efa76e30dce1bbdda97dd5af22b3373
Eterion <Eterion@users.noreply.github.com>
2022-12-28 15:05:19 +0100

    Style tier

--- src/components/BaseTier.vue
+++ src/components/BaseTier.vue
@@ -7,6 +7,7 @@ import type { Tier } from '@/types/Tier';
 import { useMouseInElement } from '@vueuse/core';
 import { computed, ref } from 'vue';
 import BaseCharacter from './BaseCharacter.vue';
+import TrashIcon from './icons/TrashIcon.vue';
 
 const props = defineProps<{
   /**
@@ -15,12 +16,15 @@ const props = defineProps<{
   info: Tier;
 }>();
 
+const CHARACTER_WIDTH = 100;
 const tierStore = useTierStore();
+
 const tier = computed(() => {
   return tierStore.tiers.find(({ id }) => {
     return id === props.info.id;
   });
 });
+
 const caption = computed({
   get: () => tier.value?.caption,
   set: (caption) => {
@@ -29,6 +33,14 @@ const caption = computed({
     }
   },
 });
+
+function onCaptionChange(event: Event) {
+  if (event.target instanceof HTMLElement) {
+    caption.value = event.target.innerText.trim();
+    event.target.innerText = caption.value;
+  }
+}
+
 function removeTier() {
   tierStore.removeTier(props.info.id);
 }
@@ -36,9 +48,11 @@ function removeTier() {
 const draggingCharacterStore = useDraggingCharacterStore();
 const rootRef = ref<HTMLElement>();
 const { isOutside } = useMouseInElement(rootRef);
+
 const isInDropArea = computed(() => {
   return draggingCharacterStore.draggingInfo && !isOutside.value;
 });
+
 draggingCharacterStore.onDrop(({ draggingInfo }) => {
   if (!isOutside.value) {
     if (draggingInfo.origin === 'search') {
@@ -66,17 +80,26 @@ const tierCharacters = computed(() => {
 
 <template>
   <div ref="rootRef" :class="[$style.el, { [$style.drop]: isInDropArea }]">
-    <div :class="[$style.caption, $style[info.color]]">
-      <input v-model.lazy="caption" type="text" />
+    <div
+      :class="[$style.tier, $style[info.color]]"
+      contenteditable
+      @blur="onCaptionChange">
+      {{ caption }}
     </div>
-    <BaseCharacter
-      v-for="tierCharacter in tierCharacters"
-      :key="tierCharacter.id"
-      :image-width="100"
-      :info="tierCharacter"
-      card
-      drag-event-origin="tier" />
-    <button type="button" @click="removeTier">Remove</button>
+    <div :class="$style.characters">
+      <BaseCharacter
+        v-for="tierCharacter in tierCharacters"
+        :key="tierCharacter.id"
+        :image-width="CHARACTER_WIDTH"
+        :info="tierCharacter"
+        card
+        drag-event-origin="tier" />
+    </div>
+    <div>
+      <button :class="$style.removeButton" type="button" @click="removeTier">
+        <TrashIcon :size="18" />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -86,26 +109,57 @@ const tierCharacters = computed(() => {
 
 .el {
   align-items: center;
+  border-radius: 12px;
   column-gap: 12px;
   display: flex;
-
+  padding: 24px;
+  transition-duration: 150ms;
+  transition-property: background-color;
   &.drop {
-    background-color: $oc-black;
+    background-color: var(--drop);
   }
 }
 
-.caption {
+.tier {
   align-items: center;
-  aspect-ratio: 4 / 5;
+  aspect-ratio: var(--aspect-ratio);
+  border-radius: 12px;
   color: $oc-white;
   display: flex;
   justify-content: center;
-  width: 100px;
+  padding: 12px;
+  text-align: center;
+  width: calc(v-bind(CHARACTER_WIDTH) * 1px);
+  &:focus {
+    outline: 2px solid var(--primary);
+  }
 }
 
 @each $-colors, $-key in $oc-color-list {
   .#{$-key} {
     background-color: map.get($-colors, '6');
+  }
+}
+
+.characters {
+  display: flex;
+  flex-grow: 1;
+  gap: 12px;
+}
+
+.removeButton {
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  border-radius: 12px;
+  color: var(--text-light);
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+  size: 46px;
+  &:hover {
+    color: var(--danger);
   }
 }
 </style>
