6e807d6d19c4eea1c7de35e515f5463c924c6730
Eterion <Eterion@users.noreply.github.com>
2022-12-30 16:49:57 +0100

    Beta badge

--- src/components/the-app/TheAppHeader.vue
+++ src/components/the-app/TheAppHeader.vue
@@ -1,6 +1,9 @@
 <template>
   <header>
-    <h1 :class="$style.title">Waifu Wars</h1>
+    <h1 :class="$style.title">
+      Waifu Wars
+      <span :class="$style.beta">Beta</span>
+    </h1>
     <p>
       We all have our favorite waifus. And of course we can scientifically prove
       it by comparing them to inferior trash.
@@ -9,8 +12,21 @@
 </template>
 
 <style module lang="scss">
+@use 'open-color/open-color' as *;
+
 .title {
   font-size: 2rem;
   margin-bottom: 0.375rem;
+}
+
+.beta {
+  background-color: transparent;
+  border: 1px solid currentColor;
+  border-radius: 21px;
+  color: $oc-orange-6;
+  font-size: 16px;
+  font-weight: normal;
+  padding: 6px 12px;
+  vertical-align: middle;
 }
 </style>
