dd6fd68c42e31c9be370eb7d6ceb9614a59fb235
Eterion <Eterion@users.noreply.github.com>
2023-01-07 15:33:00 +0100

    Update add tier buttons

--- src/components/the-app/TheAppTiers.vue
+++ src/components/the-app/TheAppTiers.vue
@@ -3,10 +3,10 @@ import { TIER_COLOR_KEYS, useTierStore } from '@/stores/useTier';
 import { confirm } from '@/utils/confirm';
 import { random } from 'lodash-es';
 import { computed } from 'vue';
-import AddTier from '../AddTier.vue';
 import BaseButton from '../BaseButton.vue';
 import BaseTier from '../BaseTier.vue';
 import ButtonGroup from '../ButtonGroup.vue';
+import PlusIcon from '../icons/PlusIcon.vue';
 
 const tierStore = useTierStore();
 
@@ -54,15 +54,63 @@ const tiers = computed(() => {
       <BaseButton @click="clearCharacters">Clear characters</BaseButton>
     </ButtonGroup>
     <template v-for="tierInfo in tiers" :key="tierInfo.id">
-      <AddTier @add="tierInfo.onAddTier" />
+      <div :class="$style.add">
+        <button
+          type="button"
+          :class="$style.add_button"
+          title="Add Tier"
+          @click="tierInfo.onAddTier">
+          <PlusIcon :size="14" />
+        </button>
+      </div>
       <BaseTier :info="tierInfo" />
     </template>
-    <AddTier @add="addTier" />
+    <div :class="$style.add">
+      <button
+        type="button"
+        :class="$style.add_button"
+        title="Add Tier"
+        @click="addTier()">
+        <PlusIcon :size="14" />
+      </button>
+    </div>
   </div>
 </template>
 
 <style module lang="scss">
 .buttons {
   margin-bottom: 24px;
+}
+
+.add {
+  padding: 6px false;
+  &_button {
+    align-items: center;
+    background-color: transparent;
+    border: none;
+    border-radius: 12px;
+    color: var(--text-light);
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    margin: false 6px;
+    padding: 6px 0;
+    transition-duration: 200ms;
+    transition-property: background-color, color, transform;
+    width: 75px - 6px * 2;
+
+    & > svg {
+      stroke-width: 1;
+    }
+
+    &:active {
+      transform: scale(0.9);
+    }
+
+    &:hover {
+      background-color: var(--gray);
+      color: var(--text);
+    }
+  }
 }
 </style>
