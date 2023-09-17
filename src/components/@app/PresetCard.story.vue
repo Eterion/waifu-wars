50038602c2fac9d74ca262188aeb3b8b367ce678
Eterion <Eterion@users.noreply.github.com>
2023-09-17 21:45:32 +0200

    Preset card

---
+++ src/components/@app/PresetCard.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PresetCard from './PresetCard.vue';
+</script>
+
+<template>
+  <Story>
+    <PresetCard
+      to="/"
+      thumbnail="https://s4.anilist.co/file/anilistcdn/character/large/b132494-R05zeZPjDG3l.jpg"
+      title="Airing Season"
+      desc="Start with waifus from the current airing season and continue from there."
+      style="width: 265px" />
+  </Story>
+</template>
---
+++ src/components/@app/PresetCard.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from 'vue-router';
+
+defineProps<{
+  /** Description text. */
+  desc: string;
+  /** Title text. */
+  title: string;
+  /** Thumbnail image url. */
+  thumbnail: string;
+  /** Route location. */
+  to: RouteLocationRaw;
+}>();
+</script>
+
+<template>
+  <RouterLink :to="to" :class="$style.el">
+    <div :class="$style.thumbnail">
+      <img :class="$style.thumbnail_img" :src="thumbnail" alt="" />
+    </div>
+    <div :class="$style.title">{{ title }}</div>
+    <p :class="$style.desc">{{ desc }}</p>
+  </RouterLink>
+</template>
+
+<style module lang="scss">
+.thumbnail {
+  aspect-ratio: 16 / 10;
+  border-radius: 12px;
+  display: block;
+  margin-bottom: 24px;
+  overflow: hidden;
+  width: 100%;
+  &_img {
+    display: block;
+    object-fit: cover;
+    size: 100%;
+    transition-duration: 200ms;
+    transition-property: transform;
+  }
+}
+
+.title {
+  font-size: 1.125rem;
+  font-weight: medium;
+  margin-bottom: 6px;
+  margin-top: 0;
+}
+
+.desc {
+  color: var(--text-light);
+  margin: 0;
+}
+
+.el {
+  color: var(--text);
+  cursor: pointer;
+  display: block;
+  text-decoration: none;
+  &:hover {
+    .thumbnail_img {
+      transform: scale(1.05);
+    }
+    .title {
+      color: var(--primary);
+      text-decoration: underline;
+    }
+  }
+}
+</style>
