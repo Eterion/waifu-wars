99b1e9f5d0b98e73f478c7911e830b1a5555044a
Eterion <Eterion@users.noreply.github.com>
2023-09-18 08:48:36 +0200

    Log click event

--- src/components/@base/button/BaseButton.story.vue
+++ src/components/@base/button/BaseButton.story.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
+import { logEvent } from 'histoire/client';
 import BaseButton from './BaseButton.vue';
 </script>
 
 <template>
   <Story>
     <Variant title="Default">
-      <BaseButton>Button</BaseButton>
+      <BaseButton @click="logEvent('click', $event)">Button</BaseButton>
     </Variant>
     <Variant title="Primary">
-      <BaseButton primary>Button</BaseButton>
+      <BaseButton primary @click="logEvent('click', $event)">Button</BaseButton>
     </Variant>
     <Variant title="Danger">
-      <BaseButton danger>Danger</BaseButton>
+      <BaseButton danger @click="logEvent('click', $event)">Danger</BaseButton>
     </Variant>
   </Story>
 </template>
