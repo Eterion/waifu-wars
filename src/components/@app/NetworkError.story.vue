2b693d22a451062c525e8ce492ade38f993fb0aa
Eterion <Eterion@users.noreply.github.com>
2023-09-20 19:22:26 +0200

    Error component

---
+++ src/components/@app/NetworkError.story.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import NetworkError from './NetworkError.vue';
+</script>
+
+<template>
+  <Story>
+    <NetworkError
+      :error="{
+        clientErrors: [],
+        extraInfo: null,
+        graphQLErrors: [],
+        message: 'An unknown error occured.',
+        name: 'Network Error',
+        networkError: null,
+        protocolErrors: [],
+      }" />
+  </Story>
+</template>
---
+++ src/components/@app/NetworkError.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { ApolloError } from '@apollo/client';
+
+defineProps<{
+  /** Error value. */
+  error: ApolloError;
+}>();
+</script>
+
+<template>
+  <div :class="$style.el">
+    <div :class="$style.name">{{ error.name }}</div>
+    <p :class="$style.message">{{ error.message }}</p>
+  </div>
+</template>
+
+<style module lang="scss">
+.el {
+  color: var(--danger);
+}
+
+.name {
+  font-size: 1.25rem;
+}
+
+.message {
+  margin: 0;
+}
+</style>
